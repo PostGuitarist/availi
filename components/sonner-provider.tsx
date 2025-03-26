684d29147cce760d22c39d35271692215b207467
Zaden Connell <zaden.connell@gmail.com>
2025-03-26 14:46:10 -0400

    Remove SonnerProvider and toast utility files

--- components/sonner-provider.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import { Toaster as SonnerToaster } from "sonner"
-
-export function SonnerProvider() {
-  return <SonnerToaster position="bottom-right" richColors closeButton theme="system" className="toaster group" />
-}
-
